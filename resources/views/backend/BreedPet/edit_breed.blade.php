1aab48f5bcbef4eb0fc62438020da6cbbb536977
blackcoffee19 <cattuongw2000@gmail.com>
2023-04-28 23:40:12 +0700

    gcnc

--- resources/views/backend/BreedPet/edit_breed.blade.php
+++ resources/views/backend/BreedPet/edit_breed.blade.php
@@ -5,22 +5,7 @@
         <div class="ms-4 bg-light p-4 rounded-2 col-6" >
             <p class="h2 text-capitalize">{{$site}} <span class="text-secondary ms-3">Breed</span></p>
             <hr>
-            @if (Session::has("error"))
-            <div class="alert alert-danger">{{Session::get('error')}}</div>
-            @endif
-            @if ($errors->has('name_breed'))
-                <div class="alert alert-danger mb-3">{{$errors->first('name_breed')}}</div>
-            @endif
-            <form action="{{$site == 'Add'? route('addbreed') : route('editbreed',[$breed_edit->id_breed])}}" method="POST">
-                @csrf
-                <div class="mb-3">
-                    <label for="type"class="form-label">Type of Pet</label>
-                    <select class="form-select" name="type" id="type">
-                        <option selected>Choose 1 type pet of breed</option>
-                        @foreach ($types as $type)
-                        <option value="{{$type->id_type}}" {{$site == "Edit" && $breed_edit->id_type_product == $type->id_type ? "selected" : ''}}>{{$type->name_type}}</option>
-                        @endforeach
-                    </select>
+            <h1>hjvjvjhv</h1>
                 </div>
                 <div class="mb-3">
                     <label class="form-label" for="name_breed">Name Breed:</label>
