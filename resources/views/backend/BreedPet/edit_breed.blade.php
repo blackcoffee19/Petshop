2e18d6563686f937f9c21850118df09738a4731e
blackcoffee19 <cattuongw2000@gmail.com>
2023-04-28 23:57:11 +0700

    df

--- resources/views/backend/BreedPet/edit_breed.blade.php
+++ resources/views/backend/BreedPet/edit_breed.blade.php
@@ -1,23 +1,8 @@
 @extends('admin')
 @section('content')
 <div class="col-sm-7 col-md-8 mx-auto mt-4">
-    <div class="row">
-        <div class="ms-4 bg-light p-4 rounded-2 col-6" >
-            <p class="h2 text-capitalize">{{$site}} <span class="text-secondary ms-3">Breed</span></p>
-            <hr>
-            <h2></h2>
-            <h2>cdasvds</h2>
-                </div>
-                <div class="mb-3">
-                    <label class="form-label" for="name_breed">Name Breed:</label>
-                    <input class="form-control" type="text" name="name_breed" id="name_breed" value="{{$site == 'Edit'? $breed_edit->breed_name:''}}" placeholder="Enter new breed of pet">
-                </div>
-                <div class="mt-3">
-                    <a href="{{route('listtype')}}" class="me-3"><i class="fa-sharp fa-solid fa-arrow-left me-2"></i>Back</a>
-                    <input type="submit" value="{{$site}} Breed" class="btn btn-primary px-3">
-                </div>
-            </form>
-        </div>
+    <div>
+        <h1>NNew</h1>
     </div>
 </div>
 @endsection
