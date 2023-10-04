f61f0cb0642d0db1b155b1e95980cc8332ed0710
blackcoffee19 <cattuongw2000@gmail.com>
2023-10-04 12:52:42 +0700

    5/10 commint from Arch

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -617,6 +617,35 @@
                     </span>
                   </a>
                 </div>
+              @else
+              <div class="list-inline-item me-5">
+                <a class="text-muted position-relative btn_showcart" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight"
+                  href="#offcanvasExample" role="button" aria-controls="offcanvasRight">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
+                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                    class="feather feather-shopping-bag">
+                    <path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z"></path>
+                    <line x1="3" y1="6" x2="21" y2="6"></line>
+                    <path d="M16 10a4 4 0 0 1-8 0"></path>
+                  </svg>
+                  <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-primary">
+                    @if (Session::has("cart"))
+                    <span class="fw-bold countCart" >
+                      @php
+                        $sum=0;
+                        foreach (Session::get('cart') as $key => $value) {
+                          $sum+=$value["amount"];
+                        }
+                        echo $sum;
+                      @endphp
+                    </span>
+                    @endif
+                    @if (!Auth::check() && !Session::has("cart"))
+                    <span class="fw-bold countCart" >0</span>
+                    @endif
+                  </span>
+                </a>
+              </div>
               @endif
               <div class="list-inline-item dropdown dropdown-fullwidth">
                   @if (Auth::check())
